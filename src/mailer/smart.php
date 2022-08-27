45144642fd24c426037e7876105fa75e727e29fa
Mikhail Tyapkov <andrimalano3@gmail.com>
2022-08-27 20:35:05 +0300

    Lesson 3.21 BackEnd-email deliver. Don't working(js-normal -no php errors)

--- src/mailer/smart.php
+++ src/mailer/smart.php
@@ -17,7 +17,7 @@ $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.mail.ru';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = 'v_tyapkov@mail.ru';                 // Наш логин
-$mail->Password = 'YavL1BaWuc4bnxFp2W8s';                           // Наш пароль от ящика
+$mail->Password = 'becouse of push';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
